5c3e10f77d769addd2b7834506b30160a4a04852
Bruno Barros Xavier <200015621@aluno.unb.br>
2023-12-01 00:21:50 -0300

    Menu Principal

--- States/TitleState/TitleState.cpp
+++ States/TitleState/TitleState.cpp
@@ -10,16 +10,16 @@
 TitleState::TitleState() {
     auto bg = new GameObject;
     auto bg_ptr = AddObject(bg);
-    bg->AddComponent(new Sprite("Assets/fundo.png", bg_ptr));
+    bg->AddComponent(new Sprite("Assets/main_menu.png", bg_ptr));
     bg->AddComponent(new CameraFollower(bg_ptr));
 
     auto txt_start_go = new GameObject;
     auto txt_start_go_ptr = AddObject(txt_start_go);
-    txt_start = new Text("Assets/Ubuntu-Regular.ttf", txt_start_go_ptr, 30, Text::SOLID, "Start", {0, 0, 0, 255});
+    txt_start = new Text("Assets/Ubuntu-Regular.ttf", txt_start_go_ptr, 40, Text::SOLID, "Start", {0, 0, 0, 255});
     txt_start_go->AddComponent(txt_start);
     Rect &fontLuxuriaBox = txt_start_go->box;
-    fontLuxuriaBox.x = GAME_WIDTH/2 - fontLuxuriaBox.w/2;
-    fontLuxuriaBox.y = GAME_HEIGHT/2 - fontLuxuriaBox.h;
+    fontLuxuriaBox.x = GAME_WIDTH/2 - fontLuxuriaBox.w/2 + 15;
+    fontLuxuriaBox.y = GAME_HEIGHT/2 + 1.7*fontLuxuriaBox.h;
 }
 
 TitleState::~TitleState() {
